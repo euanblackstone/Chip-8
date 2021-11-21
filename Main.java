1eaf41d763bf4a720bf73bd490ba3707e47646ff
euanblackstone <blackstoneeuan@gmail.com>
2021-11-21 01:34:59 -0700

    tiny change

--- Main.java
+++ Main.java
@@ -9,6 +9,9 @@ public class Main {
         try {
             Emulator chip8 = new Emulator(cpuhz, rom);
             chip8.startEmulatorLoop();
+        } catch(IOException ex) {
+            System.out.println("Rom unknown. Please use a rom from rom folder.");
+            System.exit(0);
         } catch(Exception ex) {
             ex.printStackTrace();
         }
